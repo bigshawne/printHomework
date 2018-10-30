8e7a107f68b07e676a486f3f144c2b65700595f1
bigshawne <shuwei.zhang@lawrence.edu>
2018-10-30 00:59:54 -0500

    Add the Greedy Solutions

--- Text/text.h
+++ Text/text.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include<iostream>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 template <typename T>
 class word
@@ -11,25 +12,30 @@ class word
 public:
 	T key;
 	int len;
+	int pos;
 
 	word() {};
-	word(T key, int len) { this->key = key; this->len = len; }
+	word(T key, int len, int pos) { this->key = key; this->len = len; this->pos = pos; }
 };
 
 template <typename T>
 class text
 {
 public:
-	text() :t(10000), r(10000), s(10000) {};
+	text(){};
 	text(string text);
 	~text();
-	void print(int p, int len);
+	void print(int len)
+	{
+		cout << print(0, len, INT_MAX);
+	}
 private:
 	int price(int i, int j, int len, int n);
-
+	int end(int i, int n);
+	int print(int i, int len, int q);
+	bool if_lastline(int i, int n);
 	vector<word<T>*> t;
 	int total_len = 0;
-	int remain;
 	int penalty = 0;
 	vector<int> r;
 	vector<int> s;
@@ -38,6 +44,7 @@ private:
 template <typename T>
 text<T>::text(string text)
 {
+	int pos = 0;
 	ifstream in(text);
 	string line;
 	while(getline(in, line, '\n'))
@@ -47,16 +54,16 @@ text<T>::text(string text)
 		while (getline(l, w, ' '))
 		{
 			w.push_back(' ');
-			t.push_back(new word<T>(w, w.length()));
+			t.push_back(new word<T>(w, w.length(), pos));
 			total_len += w.length();
 			total_len++;
+			pos += w.length();
 		}
 	}
 	
 	t[t.size() - 1]->key.pop_back();
 	t[t.size() - 1]->len--;
 	total_len--;
-	remain = total_len;
 }
 
 template <typename T>
@@ -70,32 +77,79 @@ text<T>::~text()
 	}
 }
 
+/*
+ *Greedy Algorithm
 template <typename T>
-void text<T>::print(int p, int len)
+void text<T>::print(int i, int len)
 {
-	int sum = 0;
-	for(unsigned int i = p; i < t.size(); ++i)
+	int line = 0;
+	for(unsigned int j = 0; j < t.size(); ++j)
 	{
-		word<T>* next = t[i];
-		sum += next->len;
-		++sum;
-		remain -= sum;
-		if(sum > len)
+		if(line + t[j]->len > len)
 		{
-			for (unsigned int j = p; j < i; j++)
-				cout << t[j]->key <<" ";
 			cout << endl;
-			break;
+			cout << t[j]->key;
+			line = 0;
+			line += t[j]->len;
+		}
+		else
+		{
+			cout << t[j]->key;
+			line += t[j]->len;
 		}
 	}
+}
+*/
+
+
+//Recursive solution
+template <typename T>
+int text<T>::print(int i, int len, int q)
+{
+	int e = end(i, len);
+	int line = 0;
+	for(int k = i; i < e; i++)
+	{
+		line += t[k]->len;
+		int p = price(i, k, line, len);
+		cout << p << endl;
+		q = min(q, p + print(k + 1, len, q));
+	}
+	return q;
+}
+
+template <typename T>
+bool text<T>::if_lastline(int i, int n)
+{
+	int remain = total_len - t[i]->pos;
+	return remain < n;
 }
 
 template <typename T>
 int text<T>::price(int i, int j, int len, int n)
 {
-	if (remain < n)
-		return 0;
+	if (if_lastline(i, n))
+		return pow(penalty, 3);
 	int p = n - j + i - len;
 	penalty += p;
 	return pow(penalty, 3);
 }
+
+template <typename T>
+int text<T>::end(int i, int n)
+{
+	unsigned int end = i;
+	int line = 0;
+	while(end < t.size())
+	{
+		if(line + t[end]->len > n)
+		{
+			return end;
+		}
+		line += t[end]->len;
+		++end;
+	}
+	return end;
+}
+
+
