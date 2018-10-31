00aca33e9448920939d0d04139dd8798b66f4b0b
bigshawne <shuwei.zhang@lawrence.edu>
2018-10-31 18:37:30 -0500

    Modify the I/O to make it accords to the question description.

--- Text/text.h
+++ Text/text.h
@@ -12,10 +12,9 @@ class word
 public:
 	T key;
 	int len;
-	int pos;
 
 	word() {};
-	word(T key, int len, int pos) { this->key = key; this->len = len; this->pos = pos; }
+	word(T key, int len) { this->key = key; this->len = len;}
 };
 
 template <typename T>
@@ -39,7 +38,6 @@ private:
 template <typename T>
 text<T>::text(string text):r(t.size(), 0), s(t.size(), 0)
 {
-	int pos = 0;
 	ifstream in(text);
 	string line;
 	while(getline(in, line, '\n'))
@@ -48,14 +46,9 @@ text<T>::text(string text):r(t.size(), 0), s(t.size(), 0)
 		T w;
 		while (getline(l, w, ' '))
 		{
-			w.push_back(' ');
-			t.push_back(new word<T>(w, w.length(), pos));
-			pos += w.length();
+			t.push_back(new word<T>(w, w.length()));
 		}
 	}
-	
-	t[t.size() - 1]->key.pop_back();
-	--t[t.size() - 1]->len;
 }
 
 template <typename T>
@@ -77,13 +70,13 @@ void text<T>::print(int len)
 	traversal();
 	unsigned int pos = 0;
 	ofstream out("result.txt");
-	while(pos < t.size() - 1)
+	while(pos < t.size())
 	{
 		int end = s[pos];
 		for(int x = pos; x <= end; ++x)
 		{
-			cout << t[x]->key;
-			out << t[x]->key;
+			cout << t[x]->key << ' ';
+			out << t[x]->key << ' ';
 		}
 		cout << endl;
 		out << endl;
@@ -95,30 +88,33 @@ void text<T>::print(int len)
 template <typename T>
 void text<T>::traversal()
 {
-	vector<int> result(t.size(), 0);
-	vector<int> seg(t.size(), 0);
-	int e = t.size() - 1;
-	for(int i =e; i > -1; --i)
+	r= vector<int>(t.size(), 0);
+	s = vector<int>(t.size(), 0);
+	for(int i = t.size() - 1; i >= 0; --i)
 	{
 		int q = INT_MAX;
-		int j = t.size() - 1;
-		for(int k = i; k <= j; ++k)
+		for(int j = i; j < t.size(); ++j)
 		{
-			if (p[i][k] >= 0) 
+			if (p[i][j] < 0)
+				break;
+			if(j == t.size() - 1)
 			{
-				if(q > min(q, p[i][k] + result[k]))
-				{
-					q = min(q, p[i][k] + result[k]);
-					seg[i] = k;
-				}
+				q = 0;
+				s[i] = j;
 			}
 			else
-				break;
+			{
+				if(q > p[i][j] + r[j + 1])
+				{
+					q = p[i][j] + r[j + 1];
+					s[i] = j;
+					if (q == 0)
+						break;
+				}
+			}
 		}
-		result[i] = q;
+		r[i] = q;
 	}
-	r = result;
-	s = seg;
 }
 
 template <typename T>
@@ -129,9 +125,9 @@ void text<T>::price()
 		p.push_back(vector<int>(t.size(), 0));
 	}
 	
-	for(unsigned int l = 0; l < t.size(); ++l)
+	for(unsigned int i = 0; i < t.size(); ++i)
 	{
-		p[l][l] = line - t[l]->len;
+		p[i][i] = line - t[i]->len;
 	}
 	p[t.size() - 1][t.size() - 1] = 0;
 	for(unsigned int m = 0; m < t.size() - 1; ++m)
@@ -144,7 +140,7 @@ void text<T>::price()
 			}
 			else 
 			{
-				p[m][y] = p[m][y - 1] - 1 - t[y]->len;
+				p[m][y] = p[m][y - 1] -1 - t[y]->len;
 			}
 		}
 	}
